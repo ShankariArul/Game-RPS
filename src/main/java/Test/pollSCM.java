2d906869e122dc930d0a9f42770ec2709c828575
shankari <shankari@localhost>
2023-06-23 20:25:10 -0700

    Testing webhook

--- src/main/java/Test/pollSCM.java
+++ src/main/java/Test/pollSCM.java
@@ -6,6 +6,7 @@ public class pollSCM {
 		// TODO Auto-generated method stub
 		System.out.println("Testing poll SCM trigger");
 		System.out.println("Testing trigger2");
+		System.out.println("Testing Webhook trigger");
 
 	}
 
