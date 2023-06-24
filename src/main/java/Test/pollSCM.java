266ded39dded901c6c313f648e32cf3243d7df7d
shankari <shankari@localhost>
2023-06-23 19:56:45 -0700

    testing

--- src/main/java/Test/pollSCM.java
+++ src/main/java/Test/pollSCM.java
@@ -5,6 +5,7 @@ public class pollSCM {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Testing poll SCM trigger");
+		System.out.println("Testing trigger2");
 
 	}
 
